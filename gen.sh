93d82f5aa16eaf1027c1f402e381acff20f4c9dc
pierre leroux <panacpp@gmail.com>
2016-11-11 19:21:56 +0100

    base 2

---
+++ gen.sh
@@ -0,0 +1,12 @@
+echo "---"
+echo "layout: post"
+echo "title : "
+echo "description : "
+echo "modified ":
+echo "tags : [  ]"
+echo "image ":
+echo "  feature: "
+echo "  credit: "
+echo "  creditlink: "
+echo "---"
+echo ""
